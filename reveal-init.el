521843c1b465fa146fd999cf6202a9f28c16bc3e
Gunther Bachmann <gunther.bachmann@itemis.de>
2017-01-24 21:07:44 +0100

    initial commit

---
+++ reveal-init.el
@@ -0,0 +1,24 @@
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
+;; (add-to-list 'package-archives
+;;              '("marmalade" .  "https://marmalade-repo.org/packages/"))
+
+(package-initialize)
+
+(setq package-selected-packages
+      '(
+    ox-reveal
+    ))
+
+(defun install-packages ()
+  "Install all required packages."
+  (interactive)
+  (unless package-archive-contents
+    (package-refresh-contents))
+  (dolist (package package-selected-packages)
+    (unless (package-installed-p package)
+      (package-install package))))
+
+(install-packages)
+(require 'ox-reveal)
